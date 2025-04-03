e1a2ba208559ec0d0ae1e7a909bbcecdca1ac9be
vladbielievtsov <vladbielievtsov@gmail.com>
2025-04-03 10:36:05 +0300

    feat: power-menu button in waybar

--- waybar/scripts/power_menu.sh
+++ waybar/scripts/power_menu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-options=" Lockin\n Power Off\n Reboot\n Logout"
+options=" Lock\n Power Off\n Reboot\n Logout"
 
 choice=$(echo -e "$options" | rofi -dmenu -p "Power Menu" -theme "~/.config/rofi/themes/power-menu.rasi")
 
 case "$choice" in
-" Lockin") hyprlock ;;
+" Lock") hyprlock ;;
 " Power Off") systemctl poweroff ;;
 " Reboot") systemctl reboot ;;
 " Logout") pkill -KILL -u $USER ;;
